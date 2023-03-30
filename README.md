4727c005414e036ac36052881f3209138b343a5a
jculleeny <jculleeny@gmail.com>
2023-03-30 15:37:35 -0700

    Improved previous button with better history and the ability to go back forward.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Web app for practicing Odyssey Kindergarten sight words that the students are currently learning.
 
 ## ToDo
-- Add more history to the "Previous" button. As is, it only can go back a single word.
+- ~~Add more history to the "Previous" button. As is, it only can go back a single word.~~
 - Allow additional words to be added.
 - Allow words to be removed.
 - Make "leveling up" animation more "fun".
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let sightWordsArray = [
   "for", "again", "many", "people", "were", "very", "your"
 ]
 
+let index = 1
 let previousWords = []
 let counterTotal = sightWordsArray.length
 let currentCount = 0
@@ -15,6 +16,7 @@ let currentCount = 0
 const sightWord = document.querySelector('.sight-word')
 const counter = document.querySelector('.counter')
 const counterWrapper = document.querySelector('.counter-wrapper')
+const nextButton = document.querySelector('#next-button')
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -23,17 +25,18 @@ function getRndInteger(min, max) {
 const updateCounter = () => {
   currentCount++
   counter.innerHTML = `${currentCount}/${counterTotal}`
-  if(currentCount <= 5) {
+
+  if(currentCount < 5) {
     counterWrapper.style.outline = "2px solid var(--red)"
-  } else if(currentCount <= 10) {
+  } else if(currentCount < 10) {
     counterWrapper.style.outline = "2px solid var(--yellow)"
-  } else if(currentCount <= 15) {
+  } else if(currentCount < 15) {
     counterWrapper.style.outline = "3px solid var(--green)"
-  } else if(currentCount <= 20) {
+  } else if(currentCount < 20) {
     counterWrapper.style.outline = "4px solid var(--green)"
-  } else if(currentCount <= 25) {
+  } else if(currentCount < 25) {
     counterWrapper.style.boxShadow = "0 0 10px var(--green)"
-  } else if(currentCount <= 30) {
+  } else if(currentCount < 30) {
     counterWrapper.style.boxShadow = "0 0 20px var(--cyan)"
   }
 }
@@ -41,34 +44,34 @@ const updateCounter = () => {
 const getFirstWord = () => {
   let index = getRndInteger(0, sightWordsArray.length - 1)
   let splitWord = sightWordsArray[index].split('')
-  let rejoinedWord = splitWord.join(`</span><span class="grow letter-block">`)
-  sightWord.innerHTML = `<span class="grow letter-block">${rejoinedWord}</span>`
+  let rejoinedWord = splitWord.join(`</span><span class="grow-word letter-block">`)
+  sightWord.innerHTML = `<span class="grow-word letter-block">${rejoinedWord}</span>`
   previousWords.push(sightWordsArray[index])
   sightWordsArray.splice(index, 1)
 }
 
 const getNewWord = () => {
-  let index = getRndInteger(0, sightWordsArray.length - 1)
-  let splitWord = sightWordsArray[index].split('')
-  let rejoinedWord = splitWord.join(`</span><span class="grow letter-block">`)
-  sightWord.innerHTML = `<span class="grow letter-block">${rejoinedWord}</span>`
-  previousWords.push(sightWordsArray[index])
-  sightWordsArray.splice(index, 1)
-
+  nextButton.innerHTML = "Next"
+  getFirstWord()
   updateCounter()
+  console.log(previousWords)
 }
 
 const getPreviousWord = () => {
-  let index = 1
+  nextButton.innerHTML = "Forward"
   sightWord.innerHTML = previousWords.at( -(index + 1))
+  index++
 }
 
-getFirstWord()
-
-// sightWord.innerHTML = getFirstWord()
+const goForwardHistory = () => {
+  sightWord.innerHTML = previousWords.at( -(index - 1))
+  index--
+}
 
 counter.innerHTML = `${currentCount}/${counterTotal}`
 
+// --- START APP ---
+getFirstWord()
 
 // --- Canvas ---
 // const c = document.getElementById("canvas")
